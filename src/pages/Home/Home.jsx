6db59a52b0b3b8d2382e3f0c91bdbdea3c856718
Cath2Bcn <catherine.goutherot@gmail.com>
2024-03-05 17:54:52 +0100

    with submit navigation from Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import "w3-css/w3.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
 
     function handleOnClick() {
-      // validating localization and criteria
+      // validating & saving localization and criteria
+      // TBD
+      console.log("handleOnClick", null);
     }
 
     return (
@@ -14,10 +17,12 @@ export default function Home() {
                 <h1 class="w3-center w3-text-grey">to specify localization of fotos</h1>
                 <h1 class="w3-center w3-text-grey">and selection criterias</h1>
                 <br/> <br/> <br/> <br/> <br/>
-                <div className="w3-container">
-                    <button class="w3-container w3-right" onClick={handleOnClick}>
-                        Continue
-                    </button>
+                <div className="w3-container w3-right">
+                    <Link class="w3-button" to="/GridFotos">
+                        <button class="w3-container" onClick={handleOnClick}>
+                            Continue
+                        </button>
+                    </Link>
                 </div>
             </div>
         </>
